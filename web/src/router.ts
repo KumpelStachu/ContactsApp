6b889bbbf26d84d7e8c9b05789893a0d2745bb51
KumpelStachu <stanislaw.rzeszut@gmail.com>
2024-07-17 15:07:39 +0200

    Fix router

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,24 +1,7 @@
-import { createRouteMask, createRouter } from '@tanstack/react-router'
+import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-const contactModalMask = createRouteMask({
-	routeTree,
-	from: '/contact/$contactId/modal',
-	to: '/contact/$contactId',
-	params: true,
-})
-
-const contactEditModalMask = createRouteMask({
-	routeTree,
-	from: '/contact/$contactId/edit/modal',
-	to: '/contact/$contactId/edit',
-	params: true,
-})
-
-export const router = createRouter({
-	routeTree,
-	routeMasks: [contactModalMask, contactEditModalMask],
-})
+export const router = createRouter({ routeTree })
 
 declare module '@tanstack/react-router' {
 	interface Register {
